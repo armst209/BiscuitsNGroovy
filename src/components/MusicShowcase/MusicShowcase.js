d42e7baa0cb032cf0eccac92821ca1b691e04199
armst209 <armst209@gmail.com>
2021-06-16 12:30:26 -0400

    more styling

--- src/components/MusicShowcase/MusicShowcase.js
+++ src/components/MusicShowcase/MusicShowcase.js
@@ -1,8 +1,10 @@
 import { React, Suspense, lazy, useState } from "react";
+import CSSTransition from "react-transition-group";
 import ReminderBackground from "../Background/ReminderBackground";
 import ReminderBackground2 from "../Background/ReminderBackground2";
 import ReminderBackground3 from "../Background/ReminderBackground3";
 import ReleaseCalendar from "../ReleaseCalendar/ReleaseCalendar";
+import AlbumPreview from "../../components/AlbumPreview/AlbumPreview";
 import "./MusicShowcaseStyles.scss";
 const LibrarySwiper = lazy(() => import("../FanPortal/LibrarySwiper"));
 
@@ -34,15 +36,27 @@ function MusicShowcase() {
             <div className="albums" onClick={showPopUp}>
               1
             </div>
-            <div className="albums">2</div>
-            <div className="albums">3</div>
-            <div className="albums">4</div>
-            <div className="albums">5</div>
-            <div className="albums">6</div>
-            <div className="albums">7</div>
-            <div className="albums">8</div>
-            <div className="albums">9</div>
-            <div className="albums">10</div>
+            <CSSTransition
+              classNames="albums"
+              in={popUp}
+              timeout={300}
+              unmountOnExit
+              onEnter={() => setPopUp(true)}
+              onExited={() => setPopUp(false)}
+            >
+              <div className="albums">2</div>
+              <div className="albums">3</div>
+              <div className="albums">4</div>
+              <div className="albums">5</div>
+              <div className="albums">6</div>
+              <div className="albums">7</div>
+              <div className="albums">8</div>
+              <div className="albums">9</div>
+              <div className="albums">10</div>
+            </CSSTransition>
+            <div>
+              <AlbumPreview />
+            </div>
           </div>
           <div className="showcase-grid-mobile">
             <Suspense>
