85507dfa595ad6207a3ae32cec16abfc73bca778
Aaron Armstrong <aarmstrong4@uwm.com>
2021-08-18 15:54:36 -0400

    fixing login popup responsiveness

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -31,9 +31,7 @@ function Login(props) {
     const handleSuccess = (res) => {
       localStorage.setItem("token", res.data.token);
       setTimeout(() => {
-        window.location.replace(
-          env.FRONTEND_URL + "/home"
-        );
+        window.location.replace(env.FRONTEND_URL + "/home");
       }, 1000);
     };
 
@@ -48,8 +46,7 @@ function Login(props) {
       setMessage("Username or Password is incorrect. Please try again.");
     };
 
-    const baseURL =
-      env.BACKEND_URL;
+    const baseURL = env.BACKEND_URL;
 
     axios({
       method: "post",
@@ -68,73 +65,75 @@ function Login(props) {
   return props.trigger ? (
     <section id="login">
       <div className="login-container">
-        <div className="login-contents">
-          <div
-            className="close-btn-login"
-            onClick={() => props.setTrigger(false)}
-          >
-            <div>X</div>
-          </div>
-          <div className="logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <h2>Sign in to Biscuits n Groovy</h2>
-          <button className="google">
-            <i className="fab fa-google"></i> <span>Sign In with Google</span>
-          </button>
-          <button className="facebook">
-            <i className="fab fa-facebook-square"></i>
-            <span>Sign In with Facebook</span>
-          </button>
-          <div className="or-fold">
-            <hr></hr>
-          </div>
-
-          <form onSubmit={submit}>
-            <div className="input-styles">
-              <input
-                type="text"
-                placeholder="Enter your username"
-                required
-                autoComplete="off"
-                onChange={(event) => {
-                  setUserName(event.target.value);
-                  // setInput(true);
-                }}
-              />
-              <input
-                type="password"
-                placeholder="Enter your password"
-                required
-                autoComplete="off"
-                onChange={(event) => setPassword(event.target.value)}
-              />
+        <div className="login-wrapper">
+          <div className="login-contents">
+            <div
+              className="close-btn-login"
+              onClick={() => props.setTrigger(false)}
+            >
+              <div>X</div>
+            </div>
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <h2>Sign in to Biscuits n Groovy</h2>
+            <button className="google">
+              <i className="fab fa-google"></i> <span>Sign In with Google</span>
+            </button>
+            <button className="facebook">
+              <i className="fab fa-facebook-square"></i>
+              <span>Sign In with Facebook</span>
+            </button>
+            <div className="or-fold">
+              <hr></hr>
             </div>
 
-            <div className="login-btn-password">
-              <div className="login-button">
-                <button className="button" type="submit">
-                  {loginStatus}
-                </button>
+            <form onSubmit={submit}>
+              <div className="input-styles">
+                <input
+                  type="text"
+                  placeholder="Enter your username"
+                  required
+                  autoComplete="off"
+                  onChange={(event) => {
+                    setUserName(event.target.value);
+                    // setInput(true);
+                  }}
+                />
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                  autoComplete="off"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
               </div>
-            </div>
-            <p>
-              <span
-                onClick={() => {
-                  props.setTrigger(false);
-                  props.showSignUp(true);
-                }}
-              >
-                New user?{" "}
-                <span className="signup-redirect">Create an account</span>
-              </span>
-            </p>
-            <div className="forgot-password">
-              <Link to="">Forgot Password?</Link>
-            </div>
-          </form>
 
-          <div className="login-messages">{message}</div>
+              <div className="login-btn-password">
+                <div className="login-button">
+                  <button className="button" type="submit">
+                    {loginStatus}
+                  </button>
+                </div>
+              </div>
+              <p>
+                <span
+                  onClick={() => {
+                    props.setTrigger(false);
+                    props.showSignUp(true);
+                  }}
+                >
+                  New user?{" "}
+                  <span className="signup-redirect">Create an account</span>
+                </span>
+              </p>
+              <div className="forgot-password">
+                <Link to="">Forgot Password?</Link>
+              </div>
+            </form>
+
+            <div className="login-messages">{message}</div>
+          </div>
         </div>
       </div>
     </section>
