b72c4816b38576f2f7984da0d2f1495076f5a5c6
Aaron Armstrong <aarmstrong4@uwm.com>
2021-08-20 18:50:20 -0400

    chaning music player

--- src/components/FanPortal/FPHomePage/Sections/AlbumPopUp/AlbumPopup.js
+++ src/components/FanPortal/FPHomePage/Sections/AlbumPopUp/AlbumPopup.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import "./AlbumPopupStyles.scss";
 import MusicPlayer from "../../../../MusicPlayer/MusicPlayer";
+import arrow_back from "../../../../../assets/images/arrow-back-yellow.svg";
 
 function AlbumPopup(props) {
   const handleClose = () => {
@@ -9,26 +10,27 @@ function AlbumPopup(props) {
 
   return (
     <section id="album-popup">
+      <div onClick={handleClose} className="close-album-info">
+        <img className="back-arrow" src={arrow_back} alt="arrow" /> Back to
+        Portal
+      </div>
       <div className="album-popup-container">
         {/* function call to close pop up */}
-        <div onClick={handleClose} className="close-album-info">
-          X
-        </div>
+
         <div className="album-popup-content">
           <img src={props.release.art_url} alt={props.release.name} />
           <h1>{props.release.name}</h1>
           <div>{props.release.description}</div>
           <div>{props.release.price}</div>
-          <div>
-            <div className="music-player">
-              <MusicPlayer
-                songs={props.release.songs}
-                albumName={props.release.name}
-                albumCover={props.release.art_url}
-              />
-            </div>
-          </div>
+          <div></div>
         </div>
+      </div>
+      <div className="music-player">
+        <MusicPlayer
+          songs={props.release.songs}
+          albumName={props.release.name}
+          albumCover={props.release.art_url}
+        />
       </div>
     </section>
   );
--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,40 +1,85 @@
 import { React, useEffect, useState } from "react";
-import "./MusicPlayerStyles.scss";
-import ReactJkMusicPlayer from "react-jinke-music-player";
-import "./PlayerStyles.css";
+// import "./MusicPlayerStyles.scss";
+// import ReactJkMusicPlayer from "react-jinke-music-player";
+// import "./PlayerStyles.css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import play_btn from "../../assets/images/compact-disc-yellow.svg";
 
 function MusicPlayer(props) {
-  const [audioLists, setAudioList] = useState([]);
+  // const [audioLists, setAudioList] = useState([]);
 
-  useEffect(() => {
-    console.log(props.songs);
-    let trackListArray = props.songs;
-    console.log(trackListArray);
-    let trackList = [];
-    trackListArray.forEach((tracks) => {
-      trackList.push({
-        cover: props.albumCover,
-        name: props.albumName,
-        musicSrc: tracks,
-      });
-    });
-    setAudioList(trackList);
-  }, [props.albumCover, props.albumName, props.songs]);
+  let trackList = props.songs;
+  console.log(props.albumName);
+  const [currentMusicIndex, setCurrentMusicIndex] = useState(0);
 
+  const handleClickPrevious = () => {
+    setCurrentMusicIndex((prevState) => ({
+      currentMusicIndex:
+        prevState.currentMusicIndex === 0
+          ? trackList.length - 1
+          : prevState.currentMusicIndex - 1,
+    }));
+  };
+  const handleClickNext = () => {
+    setCurrentMusicIndex((prevState) => ({
+      currentMusicIndex:
+        prevState.currentMusicIndex < trackList.length - 1
+          ? prevState.currentMusicIndex + 1
+          : 0,
+    }));
+  };
+
+  // useEffect((props) => {
+  //   let trackListArray = props.songs;
+  //   console.log(trackListArray);
+  //   // let trackList = [];
+  //   // trackListArray.forEach((tracks) => {
+  //   //   trackList.push({
+  //   //     cover: props.albumCover,
+  //   //     name: props.albumName,
+  //   //     musicSrc: tracks,
+  //   //   });
+  //   // });
+
+  //   // setAudioList(trackListListArray);
+  // }, [props.albumCover, props.albumName, props.songs]);
+  // console.log(audioLists[currentMusicIndex].musicSrc);
   return (
     <>
-      <ReactJkMusicPlayer
-        // icon={{ play: <img src={play_icon} /> }}
-        theme="dark"
+      <AudioPlayer
+        style={{
+          width: "100%",
+          border: "5px solid var(--color2)",
+          borderRadius: "20px",
+          background: "black",
+          fontFamily: "var(--font1)",
+          color: "white",
+          textAlign: "center",
+        }}
+        header={"Now Playing: " + props.albumName}
+        autoPlayAfterSrcChange={true}
+        showSkipControls={true}
+        src={trackList[currentMusicIndex]}
+        // onPlay={(e) => console.log("onPlay")}
+        onClickPrevious={handleClickPrevious}
+        onClickNext={handleClickNext}
+        // layout="horizontal"
+        // other props here
+        // customIcons={{ play: play_btn }}
+      />
+
+      {/* <ReactJkMusicPlayer
         preload={true}
         mode={"full"}
         autoPlay={true}
         showDownload={false}
         showThemeSwitch={false}
         showReload={false}
+        toggleMode={false}
         audioLists={audioLists}
         className="music-player-component"
-      />
+      /> */}
     </>
   );
 }
