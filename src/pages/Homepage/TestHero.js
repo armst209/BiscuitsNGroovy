6a962a01bdc5d8bd9989385c62007a4d0e33592e
Aaron Armstrong <aarmstrong4@uwm.com>
2021-08-25 13:30:51 -0400

    stlying hero section

--- src/pages/Homepage/TestHero.js
+++ src/pages/Homepage/TestHero.js
@@ -12,27 +12,30 @@ function TestHero() {
 
         <div className="hero-section-test">
           <div className="hero-section-outer-wrapper-test">
-            <div className="hero-section-inner-wrapper-test">
-              <section className="hero-test-text">
-                <div>
-                  <h1>
-                    NEW <br /> MUSIC IS
-                    <br /> OUR JAM
-                  </h1>
-                </div>
-                <div className="hero-test-subtitle">
-                  Stream exclusive music from your favorite artists, and collect
-                  NFT album art Available only for a limited time, only for true
-                  fans.
-                </div>
-                <div className="hero-test-buttons">
-                  <button className="start-btn">Start Your Collection</button>
-                  <button className="for-artists-btn">For Artists</button>
-                </div>
-              </section>
-              <section className="hero-test-image">
-                <img src={hero_image} alt="hero" />
-              </section>
+            <div className="hero-content-middle-wrapper-test">
+              {" "}
+              <div className="hero-section-inner-wrapper-test">
+                <section className="hero-test-text">
+                  <div>
+                    <h1>
+                      NEW <br /> MUSIC <br /> IS OUR <br />
+                      JAM
+                    </h1>
+                  </div>
+                  <div className="hero-test-subtitle">
+                    Stream exclusive music from your favorite artists, and
+                    collect NFT album art Available only for a limited time,
+                    only for true fans.
+                  </div>
+                  <div className="hero-test-buttons">
+                    <button className="start-btn">Start Your Collection</button>
+                    <button className="for-artists-btn">For Artists</button>
+                  </div>
+                </section>
+                <section className="hero-test-image">
+                  <img src={hero_image} alt="hero" />
+                </section>
+              </div>
             </div>
           </div>
         </div>
