4ee62d1f4a57577377b4e3bea92de310eb726686
armst209 <armst209@gmail.com>
2021-11-21 13:38:05 -0500

    fixing artist mailchimp form

--- src/pages/Artists/ArtistInformationForm.js
+++ src/pages/Artists/ArtistInformationForm.js
@@ -48,29 +48,28 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault(e);
-    artistEmail &&
-      artistName &&
-      artistPrefName &&
-      artistPhone &&
-      managerName &&
-      managerEmail &&
+    // artistEmail &&
+    artistName &&
+      // artistPrefName &&
+      // artistPhone &&
+      // managerName &&
+      // managerEmail &&
       artistMessage &&
       artistEmail.indexOf("@") > -1 &&
       onValidated({
         MERGE0: artistEmail,
         MERGE1: artistName,
-        MERGE2: artistPrefName,
-        MERGE3: artistPhone,
-        MERGE4: managerName,
-        MERGE5: managerEmail,
+        // MERGE2: artistPrefName,
+        // MERGE3: artistPhone,
+        // MERGE4: managerName,
+        // MERGE5: managerEmail,
         MERGE6: artistMessage,
       });
   };
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       {/* error,success & loading messages */}
-      {errorMessage}
-      {successMessage}
+
       {artistFormLoader && <FormLoader />}
       {/* error,success & loading messages */}
       <input
@@ -79,7 +78,7 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
         onChange={(e) => setArtistEmail(e.target.value)}
         type="email"
         value={artistEmail}
-        placeholder="Ex: your@email.com"
+        placeholder="Email: your@email.com"
         required
         autoComplete="off"
         name="MERGE0"
@@ -94,7 +93,7 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
         required
         name="MERGE1"
       />
-      <input
+      {/* <input
         label="Artist Preferred Name"
         // ref={(node) => (artistPrefName = node)}
         onChange={(e) => setArtistPrefName(e.target.value)}
@@ -103,9 +102,9 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
         placeholder="Preferred Name"
         required
         name="MERGE2"
-      />
+      /> */}
 
-      <input
+      {/* <input
         label="Phone Number"
         // ref={(node) => (artistPhone = node)}
         onChange={(e) => setArtistPhone(e.target.value)}
@@ -114,8 +113,8 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
         placeholder="Ex: 111-111-1111"
         required
         name="MERGE3"
-      />
-      <input
+      /> */}
+      {/* <input
         label="Manager/Agent Name"
         // ref={(node) => (managerName = node)}
         onChange={(e) => setManagerName(e.target.value)}
@@ -131,11 +130,12 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
         onChange={(e) => setManagerEmail(e.target.value)}
         type="email"
         value={managerEmail}
-        placeholder="Ex: manageragent@email.com"
+        placeholder="Email: manageragent@email.com"
         required
         name="MERGE5"
-      />
-      <input
+      /> */}
+      <textarea
+        className="message-input"
         label="Enter a message:"
         // ref={(node) => (artistMessage = node)}
         onChange={(e) => setArtistMessage(e.target.value)}
@@ -147,6 +147,8 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
       />
 
       <button type="submit">Submit</button>
+      {errorMessage}
+      {successMessage}
     </form>
   );
 };
