e4cc706d4f24d13ae7083a8b1f1fdbcfb393a6e8
Aaron Armstrong <aarmstrong4@uwm.com>
2021-12-17 18:14:46 -0500

    site wide styling, forms validation implementation - artists & newsletter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,6 @@ function Footer() {
               <ul>
                 <h4>Reach Out</h4>
                 <Link to="/artists">Artists</Link>
-                <Link to="/events">Events</Link>
                 <a href="mailto:support@bngroovy.com?subject=Support">
                   Support
                 </a>
--- src/components/MailchimpForms/ArtistInformation/ArtistInformationForm.js
+++ src/components/MailchimpForms/ArtistInformation/ArtistInformationForm.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import useValidation from "../../../customHooks/Validation/useValidation";
+import "../../../customHooks/Validation/useValidationStyles.scss";
 import FormLoader from "../../Loading/Forms/FormLoader";
+import { ReactComponent as ValidationSuccess } from "../../../assets/images/check.svg";
 
 const ArtistInformationForm = ({ status, message, onValidated }) => {
   const [artistName, setArtistName] = useState("");
   const [artistPrefName, setArtistPrefName] = useState("");
-  const [artistEmail, setArtistEmail] = useState("");
   const [artistPhone, setArtistPhone] = useState("");
   const [managerName, setManagerName] = useState("");
   const [managerEmail, setManagerEmail] = useState("");
@@ -15,28 +17,41 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
   const [successMessage, setSuccessMessage] = useState("");
   const [artistFormLoader, setArtistFormLoader] = useState(false);
 
+  const {
+    email,
+    setEmail,
+    emailErrorMessage,
+    emailInputLoginClass,
+    showNewsletterArtistEmailValidationCheck,
+    setShowNewsletterArtistEmailValidationCheck,
+    setEmailInputLoginClass,
+    inputValidation,
+  } = useValidation();
+
   useEffect(() => {
     if (status === "success") {
       setArtistFormLoader(false);
       clearFields();
+      setShowNewsletterArtistEmailValidationCheck(false);
       setSuccessMessage(
-        <div className="form-success">
-          Success! Thank you for submitting your information!
-        </div>
+        <div className="form-success">Success! Thank you for subscribing!</div>
       );
+      setEmailInputLoginClass("");
+      setErrorMessage("");
     } else if (status === "error") {
       setArtistFormLoader(false);
-      // setEmailInputErrorClass("input-error");
+      setEmailInputLoginClass("input-error");
       setErrorMessage(
-        <div className="form-error">{`${artistName} has already been submitted as an artist!`}</div>
+        <div className="form-error">{`${email} is already subscribed!`}</div>
       );
-    } else if (status == "sending") {
+      setSuccessMessage("");
+    } else if (status === "sending") {
       setArtistFormLoader(true);
     }
   }, [status]);
 
   const clearFields = () => {
-    setArtistEmail("");
+    setEmail("");
     setArtistName("");
     setArtistPrefName("");
     setArtistPhone("");
@@ -47,16 +62,16 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault(e);
-    artistEmail &&
-      artistName &&
+
+    artistName &&
       artistPrefName &&
       artistPhone &&
       managerName &&
       managerEmail &&
       artistMessage &&
-      artistEmail.indexOf("@") > -1 &&
+      email &&
       onValidated({
-        MERGE0: artistEmail,
+        MERGE0: email,
         MERGE1: artistName,
         MERGE2: artistPrefName,
         MERGE3: artistPhone,
@@ -72,79 +87,84 @@ const ArtistInformationForm = ({ status, message, onValidated }) => {
       {successMessage}
       {artistFormLoader && <FormLoader />}
       {/* error,success & loading messages */}
-      <input
-        label="Email"
-        // ref={(node) => (email = node)}
-        onChange={(e) => setArtistEmail(e.target.value)}
-        type="email"
-        value={artistEmail}
-        placeholder="Ex: your@email.com"
-        required
-        autoComplete="off"
-        name="MERGE0"
-      />
-      <input
-        label="Artist Name"
-        // ref={(node) => (artistName = node)}
-        onChange={(e) => setArtistName(e.target.value)}
-        type="text"
-        value={artistName}
-        placeholder="Artist Name"
-        required
-        name="MERGE1"
-      />
-      <input
-        label="Artist Preferred Name"
-        // ref={(node) => (artistPrefName = node)}
-        onChange={(e) => setArtistPrefName(e.target.value)}
-        type="text"
-        value={artistPrefName}
-        placeholder="Preferred Name"
-        name="MERGE2"
-      />
+      <fieldset className="input-styles">
+        <label
+          className="label-error-message artist-email-label"
+          htmlFor="artist-email"
+        >
+          {emailErrorMessage}
+        </label>
+        <input
+          className={emailInputLoginClass}
+          onChange={inputValidation}
+          id="artist-email"
+          type="email"
+          placeholder="Ex: your@email.com"
+          required
+          autoComplete="off"
+          name="MERGE0"
+        />
+        {/* check icon */}
+        {showNewsletterArtistEmailValidationCheck && (
+          <ValidationSuccess className="valid-check-icon email-check" />
+        )}
+        <input
+          label="Artist Name"
+          onChange={(e) => setArtistName(e.target.value)}
+          type="text"
+          value={artistName}
+          placeholder="Artist Name"
+          required
+          name="MERGE1"
+        />
+        <input
+          label="Artist Preferred Name"
+          onChange={(e) => setArtistPrefName(e.target.value)}
+          type="text"
+          value={artistPrefName}
+          placeholder="Preferred Name"
+          name="MERGE2"
+        />
 
-      <input
-        label="Phone Number"
-        // ref={(node) => (artistPhone = node)}
-        onChange={(e) => setArtistPhone(e.target.value)}
-        type="tel"
-        value={artistPhone}
-        placeholder="Ex: 111-111-1111"
-        required
-        name="MERGE3"
-      />
-      <input
-        label="Manager/Agent Name"
-        // ref={(node) => (managerName = node)}
-        onChange={(e) => setManagerName(e.target.value)}
-        type="text"
-        value={managerName}
-        placeholder="Manager/Agent Name"
-        required
-        name="MERGE4"
-      />
-      <input
-        label="Manager/Agent Email"
-        // ref={(node) => (managerEmail = node)}
-        onChange={(e) => setManagerEmail(e.target.value)}
-        type="email"
-        value={managerEmail}
-        placeholder="Ex: manageragent@email.com"
-        required
-        name="MERGE5"
-      />
-      <input
-        label="Enter a message:"
-        // ref={(node) => (artistMessage = node)}
-        onChange={(e) => setArtistMessage(e.target.value)}
-        type="text"
-        value={artistMessage}
-        placeholder="Enter a message:"
-        required
-        name="MERGE6"
-      />
+        <input
+          label="Phone Number"
+          onChange={(e) => setArtistPhone(e.target.value)}
+          type="tel"
+          value={artistPhone}
+          placeholder="Ex: 111-111-1111"
+          required
+          name="MERGE3"
+        />
+        <input
+          label="Manager/Agent Name"
+          onChange={(e) => setManagerName(e.target.value)}
+          type="text"
+          value={managerName}
+          placeholder="Manager/Agent Name"
+          required
+          name="MERGE4"
+        />
+        <input
+          label="Manager/Agent Email"
+          onChange={(e) => setManagerEmail(e.target.value)}
+          type="email"
+          value={managerEmail}
+          placeholder="Ex: manageragent@email.com"
+          required
+          name="MERGE5"
+        />
+        <input
+          label="Enter a message:"
+          onChange={(e) => setArtistMessage(e.target.value)}
+          type="text"
+          value={artistMessage}
+          placeholder="Enter a message:"
+          required
+          name="MERGE6"
+        />
 
-      <button>Submit</button>
+        <button>Submit</button>
+      </fieldset>
     </form>
   );
 };
