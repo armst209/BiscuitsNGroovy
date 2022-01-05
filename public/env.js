a70043d51aa34786593749b9298aeea52b8ec775
Erich Shan <ershan@umich.edu>
2022-01-05 20:02:57 +0100

    fix flow cors issue

--- public/env.js
+++ public/env.js
@@ -1,5 +1,6 @@
 window.env = {
-  "REACT_APP_ACCESS_NODE": "https://access-testnet.onflow.org",
-  "REACT_APP_WALLET_DISCOVERY": "https://fcl-discovery.onflow.org/testnet/authn",
-  "REACT_APP_CONTRACT_PROFILE": "0xf7ebe30e2e33b1f2"
+  "REACT_APP_ACCESS_NODE": "https://access-mainnet-beta.onflow.org",
+  "REACT_APP_WALLET_DISCOVERY": "https://flow-wallet.blocto.app/authn",
+  "REACT_APP_CONTRACT_PROFILE": "0x7859c48816bfea3c",
+  "ENV": "prod"
 };
