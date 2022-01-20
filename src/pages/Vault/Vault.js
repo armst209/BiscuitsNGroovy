13d7723bfee278925f7c186d7772a3dbf45ba446
Michael Tran <michaelhtran120@gmail.com>
2022-01-20 09:36:59 -0800

    update breakpoint max-width:400px to 500px

--- src/pages/Vault/Vault.js
+++ src/pages/Vault/Vault.js
@@ -1,18 +1,17 @@
 // react imports
-import { useEffect, useRef, useState } from 'react'
-import { HashLink } from 'react-router-hash-link'
-import useFetch from '../../customHooks/Fetch/useFetch';
+import { useEffect, useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import useFetch from "../../customHooks/Fetch/useFetch";
 import ReleaseImage from "../../components/ReleaseContent/ReleaseComponents/ReleaseImage/ReleaseImage";
 
 //styles
 import styles from "./Vault.module.scss";
 
 //Component imports
-import ComponentLoading from '../../components/Loading/Component/ComponentLoading';
+import ComponentLoading from "../../components/Loading/Component/ComponentLoading";
 import FixedNavigationSpacer from "../../components/FixedNavigationSpacer/FixedNavigationSpacer";
 import { ReactComponent as RecordVinyl } from "../../assets/images/compact-disc-yellow.svg";
-import VaultReleaseModal from './VaultReleaseModal';
-import NoReleasesMusicShowcase from '../../components/ReleaseContent/ReleaseComponents/NoReleases/NoReleasesMusicShowcase';
+import VaultReleaseModal from "./VaultReleaseModal";
 
 
 const Vault = () => {
@@ -20,26 +19,21 @@ const Vault = () => {
   const [isReleaseModalOpen, setIsReleaseModalOpen] = useState(false);
   const [selectedRelease, setSelectedRelease] = useState(null);
 
-  const {
-    responseData: releaseData,
-    isLoading,
-    errorMessage,
-  } = useFetch(`${process.env.REACT_APP_BACKEND_URL}/vault`);
-
+  const { responseData: releaseData, isLoading, errorMessage } = useFetch(`${process.env.REACT_APP_BACKEND_URL}/vault`);
 
   useEffect(() => {
     if (typeof releaseData === "object" && releaseData !== null) {
       // convert object of objects to array of objects.
-      let objToArr = Object.keys(releaseData).map(key => {
+      let objToArr = Object.keys(releaseData).map((key) => {
         return releaseData[key];
-      })
-      setReleaseArr(objToArr)
+      });
+      setReleaseArr(objToArr);
     }
-  }, [releaseData])
+  }, [releaseData]);
 
   useEffect(() => {
     console.log(selectedRelease);
-  }, [selectedRelease])
+  }, [selectedRelease]);
 
   // Release onclick method
   const handleReleaseClick = (release) => {
@@ -50,27 +44,34 @@ const Vault = () => {
   const handleModalClose = () => {
     setSelectedRelease(null);
     setIsReleaseModalOpen(false);
-  }
+  };
 
   // Render releases in Vault container
   const mapReleases = (array) => {
-    return array.map(release => {
+    return array.map((release) => {
       const { artist_name, release_art, release_id } = release;
       return (
-        <div key={release_id} className={styles["release-wrapper"]} onClick={() => handleReleaseClick(release)}>
-          <ReleaseImage releaseImageSrc={release_art} releaseAlt="random alt" />
+        <div 
+          key={release_id} 
+          className={styles["release-wrapper"]} 
+          onClick={() => handleReleaseClick(release)}>
+          <ReleaseImage 
+            releaseImageSrc={release_art} 
+            releaseAlt="random alt" />
           <div className={styles["release-overlay-container"]}>
             <h2>{artist_name}</h2>
           </div>
         </div>
-      )
-    })
+      );
+    });
   };
 
   return (
     <>
       <FixedNavigationSpacer />
-      <section id="vault" className={styles["vault"]}>
+      <section 
+        id="vault" 
+        className={styles["vault"]}>
         <div className={styles["vault-title"]}>
           <h1>
             <RecordVinyl width="50px" />
@@ -80,17 +81,18 @@ const Vault = () => {
         <div className={styles["vault-contents-wrapper"]}>
           <div className={styles["vault-header"]}>
             <h1>
-              The vault is a repository of all <span>past</span> releases on BnG. <br /> Take a look at the <span>artist</span> we have worked with thus far.
+              The vault is a repository of all <span>past</span> releases on BnG. <br /> Take a look at the <span>artist</span> we have worked with
+              thus far.
               <br />
               <br />
-              To see releases currently available, visit the
-              {' '}
-              <HashLink
-                className={styles["showcase-link"]}
-                smooth
+              To see releases currently available, visit the{" "}
+              <HashLink 
+                className={styles["showcase-link"]} 
+                smooth 
                 to="/#music-showcase-return">
                 music showcase
-              </HashLink>.
+              </HashLink>
+              .
             </h1>
           </div>
           <div className={styles["vault-showcase"]}>
@@ -99,14 +101,15 @@ const Vault = () => {
           </div>
         </div>
       </section>
-      {selectedRelease ? <VaultReleaseModal
-        isModalOpen={isReleaseModalOpen}
-        release={selectedRelease}
-        handleModalClose={handleModalClose}
-      /> : <></>}
-      <NoReleasesMusicShowcase/>
+      {selectedRelease ? 
+      <VaultReleaseModal 
+        isModalOpen={isReleaseModalOpen} 
+        release={selectedRelease} 
+        handleModalClose={handleModalClose} /> 
+      : 
+      <></>}
     </>
-  )
-}
+  );
+};
 
-export default Vault
+export default Vault;
