07e05ae9256a6dbb633992abe8dd835dc728d7ae
Aaron Armstrong <60587878+armst209@users.noreply.github.com>
2022-04-05 13:21:52 -0400

    adding GENERATE_SOURCEMAP to env.prod

--- src/utils/SEO/SEOHelmet.js
+++ src/utils/SEO/SEOHelmet.js
@@ -1,21 +1,21 @@
 //react helmet
-import { Helmet } from "react-helmet-async"
+import { Helmet } from "react-helmet-async";
 
 //react imports
 import { useRouteMatch } from "react-router-dom";
 
-
 const SEOHelmet = ({ title, content }) => {
+  const route = useRouteMatch();
 
-    const route = useRouteMatch();
+  return (
+    <Helmet>
+      <title>{title} | Biscuits n Groovy</title>
+      <meta name="description" content={content} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={`${title} | Biscuits n Groovy`} />
+      <link rel="canonical" href={`${route.path}`} />
+    </Helmet>
+  );
+};
 
-    return (
-        <Helmet>
-            <title>{title} | Biscuits n Groovy</title>
-            <meta name="description" content={content} />
-            <link rel="canonical" href={`${route.path}`} />
-        </Helmet>
-    )
-}
-
-export default SEOHelmet
+export default SEOHelmet;
--- src/utils/UtilityVariables.js
+++ src/utils/UtilityVariables.js
@@ -1,2 +1,4 @@
-export let token = !JSON.parse(localStorage.getItem("bng_user")) ? null : JSON.parse(localStorage.getItem("bng_user")).authentication.token;
+export let token = !JSON.parse(localStorage.getItem("bng_user"))
+  ? null
+  : JSON.parse(localStorage.getItem("bng_user")).authentication.token;
 export let PR_Auth_Token = localStorage.getItem("PR_Auth_Token");
